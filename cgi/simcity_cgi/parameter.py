7c9f92157b2a296d28b0ee7a3c29dff7ae93e68a
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-02-16 10:01:46 +0100

    Added Choice parameter

--- cgi/simcity_cgi/parameter.py
+++ cgi/simcity_cgi/parameter.py
@@ -25,45 +25,39 @@ def parse_parameters(parameters, parameter_specs):
                 abort(400, "value of " + str(value) + " for parameter " + name + " does not comply to " + str(spec))
     return params
 
-
-TYPE_STR = {
-    'int': int,
-    'float': float,
-    'str': str
-}
-
-def extract_and_convert(value, type_str):
-    try:
-        return TYPE_STR[type_str](value)
-    except KeyError:
-        raise ValueError("Type '" + type_str + "' unknown")
-
-def parse_parameter_spec(idict):
+def parse_parameter_spec(idict):        
+    default = idict.get('default', default=None)
     if idict['type'] == 'interval':
-        try:
-            dtype = idict['dtype']
-        except:
-            dtype = 'float'
-        min = extract_and_convert(idict['min'], dtype)
-        max = extract_and_convert(idict['max'], dtype)
-        try:
-            default = extract_and_convert(idict['default'], dtype)
-        except:
-            default = None
-        return Interval(min, max, default)
+        dtype = idict.get('dtype', default='float')
+        return Interval(idict['min'], idict['max'], default, dtype)
+    if idict['type'] == 'choice':
+        dtype = idict.get('dtype', default='str')
+        return Choice(idict['choices'], default, dtype)
         
     raise ValueError('parameter type not recognized')
 
 class ParameterSpec(object):
-    def __init__(self, default):
-        self._default = default
+    TYPE_STR = {
+        'int': int,
+        'float': float,
+        'str': str
+    }
+    def __init__(self, default, dtype):
+        if type(dtype) == str:
+            try:
+                self._dtype = ParameterSpec.TYPE_STR[dtype]
+            except KeyError:
+                raise ValueError("Type " + dtype + " unknown; use one of " + str(ParameterSpec.keys()))
+        else:
+            self._dtype = dtype
+        self._default = self.coerce(default)
     
     @property
     def default(self):
         return self._default
     @property
     def dtype(self):
-        return type(self.default)
+        return self._dtype
         
     def coerce(self, value):
         if type(value) == self.dtype:
@@ -74,22 +68,47 @@ class ParameterSpec(object):
     def is_valid(self, value):
         raise NotImplementedError
 
+class Choice(ParameterSpec):
+    def __init__(self, choices, default, dtype):
+        if type(choices) != list:
+            raise ValueError("Choices must be provided as a list")
+        if len(choices) == 0:
+            raise ValueError("At least one choice is required.")
+        if default is None:
+            default = choices[0]
+        
+        super(Choice, self).__init__(default, dtype)
+        self._choices = [self.coerce(choice) for choice in choices]
+        self._choices.sort()
+    
+    @property
+    def choices(self):
+        return self._choices.copy()
+    
+    def is_valid(self, value):
+        return type(value) == self.dtype and value in self._choices
+    
+    def __str__(self):
+        return 'choice ' + str(self._choices) + ' ' + str(self.dtype)
+        
+    def __eq__(self, other):
+        return type(self) == type(other) and self._choices == other._choices and self.default == other.default and self.dtype == other.dtype
+    
+    def __hash__(self):
+        return make_hash(self._default, self.dtype, *self._choices)
+        
 class Interval(ParameterSpec):
-    def __init__(self, min, max, default):
+    def __init__(self, min, max, default, dtype):
         if default is None:
             default = (self._min + self._max) / 2
-        super(Interval, self).__init__(default)
+        super(Interval, self).__init__(default, dtype)
         
-        if type(min) != self.dtype:
-            raise ValueError("Type of minimum not compatible with " + str(self.dtype))
-        if type(max) != self.dtype:
-            raise ValueError("Type of maximum not compatible with " + str(self.dtype))
-        if min > max:
-            raise ValueError("Minimum to an interval must be smaller than maximum")
-            
-        self._min = min
-        self._max = max
+        self._min = self.coerce(min)
+        self._max = self.coerce(max)
 
+        if self._min > self._max:
+            raise ValueError("Minimum to an interval must be smaller than maximum")
+        
         if not self.is_valid(self.default):
             raise ValueError("default value for interval not valid")
         
@@ -99,9 +118,6 @@ class Interval(ParameterSpec):
     @property
     def min(self):
         return self._min
-    @property
-    def default(self):
-        return self._default
     
     def is_valid(self, value):
         return value >= self.min and value <= self.max and type(value) == self.dtype
@@ -110,7 +126,7 @@ class Interval(ParameterSpec):
         return 'interval [' + str(self.min) + ',' + str(self.max) + '] ' + str(self.dtype)
         
     def __eq__(self, other):
-        return type(self) == type(other) and self.min == other.min and self.max == other.max and self.dtype == other.dtype
+        return type(self) == type(other) and self._min == other._min and self._max == other._max and self._default == other._default and self.dtype == other.dtype
     
     def __hash__(self):
-        return make_hash(self._min, self._max, self.dtype)
+        return make_hash(self._min, self._max, self._default, self.dtype)
