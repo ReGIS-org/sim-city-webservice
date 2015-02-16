abf7c83763dfa8b357b38628162dacd83bfacfab
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-02-16 12:38:58 +0100

    Added job submission to web service

--- cgi/simcity_cgi/app.py
+++ cgi/simcity_cgi/app.py
@@ -37,7 +37,7 @@ def simulate_name_version( name, version = None ):
         'input': params
     })
     
-    couch_cfg = simcity.config.section('CouchDB')
+    couch_cfg = simcity.config.section('task-db')
     response.status = 201 # created
     response.set_header('Location', couch_cfg['url'] + couch_cfg['database'] + '/' + token.id)
     return token.value
@@ -52,5 +52,24 @@ def overview():
         return simcity.overview_total()
     except:
         return error(500, "cannot read overview")
+
+@post('/app/job')
+def submit_job():
+    return submit_job_to_host(config_sim['default_host'])
+
+@post('/app/job/<host>')
+def submit_job_to_host(host):
+    try:
+        job = simcity.job.submit_if_needed(host, config_sim['max_jobs'])
+    except ValueError:
+        return error(404, "Host " + host + " unknown")
+    except IOError:
+        return error(502, "Cannot connect to host")
+    else:
+        if job is None:
+            response.status = 503 # service temporarily unavailable
+        else:
+            response.status = 201 # created
+            return {key: job[key] for key in ['_id', 'batch_id', 'hostname']}
 
 run(host='localhost', port=9090, debug=True, reloader=True)
