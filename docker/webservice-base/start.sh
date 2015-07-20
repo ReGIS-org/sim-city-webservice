4bff2909766774bcbb7c473b7e0a906555c5b18e
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-07-20 13:06:36 +0200

    Tweaks for creating database

--- docker/webservice-base/start.sh
+++ docker/webservice-base/start.sh
@@ -4,7 +4,12 @@
 # cut off tcp:// from tcp://ip.ad.dr.ss:port
 export COUCHDB_HOST="${COUCHDB_PORT:6}"
 export OSMIUM_HOST="${OSMIUM_PORT:6}"
-echo ${COUCHDB_USERNAME:-docker}
-echo ${COUCHDB_PASSWORD:-docker}
+if [ -z "$COUCHDB_USERNAME" ]; then
+  echo "Warning: COUCHDB_USERNAME not set: using docker"
+fi
+if [ -z "$COUCHDB_PASSWORD" ]; then
+  echo "Warning: COUCHDB_PASSWORD not set: using docker"
+fi
+
 python env/src/simcity/scripts/createDatabase.py ${COUCHDB_USERNAME:-docker} -p ${COUCHDB_PASSWORD:-docker} &&
   exec python -m bottle scripts.app --bind 0.0.0.0:9090 -s gevent
