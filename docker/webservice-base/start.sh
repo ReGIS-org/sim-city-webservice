5d75799f244f69f1d0a4f0c5a013876bf6c6b62c
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-07-28 15:38:13 +0200

    Minor changes for make docker-run

--- docker/webservice-base/start.sh
+++ docker/webservice-base/start.sh
@@ -11,5 +11,12 @@ if [ -z "$COUCHDB_PASSWORD" ]; then
   echo "Warning: COUCHDB_PASSWORD not set: using docker"
 fi
 
+while ! curl http://$COUCHDB_HOST/
+do
+  echo "Waiting for Couchdb..."
+  sleep 1
+done
+echo "$(date) - connected successfully"
+
 python env/src/simcity/scripts/createDatabase.py ${COUCHDB_USERNAME:-docker} -p ${COUCHDB_PASSWORD:-docker} &&
   exec python -m bottle scripts.app --bind 0.0.0.0:9090 -s gevent
