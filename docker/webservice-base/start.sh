7d72c09c756515239e0d4fbc47f171d0ae4dbe35
Berend Weel <b.weel@esciencecenter.nl>
2016-09-28 16:07:35 +0200

    updated start script

--- docker/webservice-base/start.sh
+++ docker/webservice-base/start.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
 . env/bin/activate
-# cut off tcp:// from tcp://ip.ad.dr.ss:port
-export COUCHDB_HOST="${COUCHDB_PORT:6}"
-export OSMIUM_HOST="${OSMIUM_PORT:6}"
-if [ -z "$COUCHDB_USERNAME" ]; then
-  echo "Warning: COUCHDB_USERNAME not set: using docker"
-fi
-if [ -z "$COUCHDB_PASSWORD" ]; then
-  echo "Warning: COUCHDB_PASSWORD not set: using docker"
-fi
-
-while ! curl http://$COUCHDB_HOST/
+while ! curl -s http://taskdb:5984/
 do
-  echo "Waiting for Couchdb..."
+  echo "Waiting for task db..."
   sleep 1
 done
-echo "$(date) - connected successfully"
+while ! curl -s http://jobdb:5984/
+do
+  echo "Waiting for job db..."
+  sleep 1
+done
+TIME=`date`
+echo "$TIME - connected successfully"
 
-simcity init -u ${COUCHDB_USERNAME:-docker} -p ${COUCHDB_PASSWORD:-docker} &&
+simcity init -u simcityadmin -p simcity &&
   exec python -m bottle scripts.app --bind 0.0.0.0:9090 -s gevent
